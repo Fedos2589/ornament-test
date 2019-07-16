502d0cf1f28aa66faea3e0ff902a57298af57a57
Gromov Fyodor <FGromov@alfabank.ru>
2019-07-16 16:10:44 +0300

    feat(tests): tests

--- src/components/addItem/addItem.test.js
+++ src/components/addItem/addItem.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AddItem from './addItem.tsx';
+import { Fab } from '@material-ui/core';
 import enzyme, { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const wrapper = mount(<AddItem />)
+describe('content', () => {
+  test('renders without crashing', () => {
+    const component = mount(<AddItem />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test('should call addRecord handler', () => {
+    const onClick = jest.fn();
+    const component = mount(<AddItem addRecord={ onClick } />);
+
+    (component.find(Fab)).simulate('click');
+    expect(onClick).toHaveBeenCalled();
+  });
 });
--- src/components/todoItem/todoItem.test.js
+++ src/components/todoItem/todoItem.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoItem from './todoItem.tsx';
+import { Create, DeleteSweep } from '@material-ui/icons';
+import Input from '@material-ui/core/Input';
 import enzyme, { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const wrapper = mount(<TodoItem />)
+const record = { text: 'sdfbg', done: true, index: 0};
+const deleteRecord = jest.fn();
+const updateRecord = jest.fn();
+
+describe('content', () => {
+  test('renders without crashing', () => {
+    const component = mount(<TodoItem />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test('should render Input on update button click', () => {
+    const component = mount(
+      <TodoItem {...record} deleteRecord={ deleteRecord } updateRecord={ updateRecord } />
+    );
+
+    expect(component.find(Input)).toHaveLength(0);
+    component.find(Create).simulate('click');
+    expect(component.find(Input)).toHaveLength(1);
+  });
+
+  test('should call deleteRecord handler', () => {
+    const component = mount(
+      <TodoItem {...record} deleteRecord={ deleteRecord } updateRecord={ updateRecord } />
+    );
+
+    component.find(DeleteSweep).simulate('click');
+    expect(deleteRecord).toHaveBeenCalled();
+  });
 });
