13c10c90868b91c4babdcdd52a4101c4ecd426da
Gromov Fyodor <FGromov@alfabank.ru>
2019-07-16 16:52:39 +0300

    Updates

---
+++ precache-manifest.a51d7702815cb316baac89c63a1f93f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21941e3aabcb185f4587318a904be10d",
+    "url": "/ornament-test/index.html"
+  },
+  {
+    "revision": "4694d27fa13d74c67eb5",
+    "url": "/ornament-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7f50c89705fb844a10c9",
+    "url": "/ornament-test/static/js/2.813e971d.chunk.js"
+  },
+  {
+    "revision": "4694d27fa13d74c67eb5",
+    "url": "/ornament-test/static/js/main.9f1750fc.chunk.js"
+  },
+  {
+    "revision": "6c94d89b0e7187268f13",
+    "url": "/ornament-test/static/js/runtime~main.b9bfa2ca.js"
+  }
+]);
